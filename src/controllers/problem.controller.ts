3ce2cbc29d41cd39272875cb99abffb0f35c5e03
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-06-03 21:29:14 +0530

    Validate reference solutions with Judge0

--- src/controllers/problem.controller.ts
+++ src/controllers/problem.controller.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import problem from "../models/problem.js";
-import { languageId } from "../utils/judge0.js";
+import { languageId, submitBatch, submitToken } from "../utils/judge0.js";
+
 
 type ProblemTag = "array" | "Linkedlist" | "graph" | "dp";
 
@@ -22,13 +23,13 @@ interface RequestBody {
         javascript: string,
         python: string,
         java: string,
-        cpp:string,
+        cpp: string,
     };
     referencesolution: {
         javascript: string,
         python: string,
         java: string,
-        cpp:string,
+        cpp: string,
     };
 
 }
@@ -57,31 +58,56 @@ export const CreateProblem = async (req: Request<{}, {}, RequestBody>, res: Resp
             referencesolution
         } = req.body
 
-        for(const language in referencesolution){
-            const langid = languageId(language);
+        type languageis = "javascript" | "python" | "java" | "cpp";
+
+        interface Judge0Submission {
+            source_code: string;
+            language_id: number;
+            stdin: string;
+            expected_output: string;
         }
 
 
+        for (const language in referencesolution) {
+            const langid = languageId(language);
+            const submissions: Judge0Submission[] = VisibleTestCases.map(({ input, output }) => ({
+                source_code: referencesolution[language as languageis],
+                language_id: langid,
+                stdin: input,
+                expected_output: output,
+            }));
 
+            const submitResult = await submitBatch(submissions);
 
+            interface SubmitResult {
+                token: string;
+            };
 
+            const resulttoken = submitResult.map((element: SubmitResult) => {
+                return element.token;
+            })
 
+            const testResult = await submitToken(resulttoken);
 
+            for (const element of testResult){
+                 if(element.status_id!==3){
+                     res.status(400).send({
+                        message:"Error Occured",
+                    })
+                return;
+                }
+            }
 
+        } 
 
-
-
-
-
-
-
-
-
-    } catch (error) {
-        if (error instanceof Error) {
+    }
+    catch(error) {
+            if (error instanceof Error) {
+                res.send({
+                    message:error.message
+                })
+            }
 
         }
 
     }
-
-}

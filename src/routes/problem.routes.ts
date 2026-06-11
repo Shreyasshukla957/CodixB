a8b761f54b1fc2ceb02c912bc3cbfd7eb9b8d340
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-06-11 13:26:40 +0530

    add multiple problem routes for problem handlers

--- src/routes/problem.routes.ts
+++ src/routes/problem.routes.ts
@@ -6,6 +6,8 @@ import { UpdateProblem } from "../controllers/update.controller.js";
 import { DeleteProblem } from "../controllers/delete.controller.js";
 import { FetchProblem } from "../controllers/FetchProblem.controller.js";
 import { FetchAllProblem } from "../controllers/FetchProblem.controller.js";
+import { SolvedProblem } from "../controllers/solvedproblem.controller.js";
+import { totalproblemsubmitted } from "../controllers/problemsubmitted.controller.js";
 const problemrouter: Router = Router();
 
 
@@ -13,12 +15,13 @@ const problemrouter: Router = Router();
 
 // Admin access is Required
 problemrouter.post("/create", adminMiddleware, CreateProblem);
-problemrouter.put("/update/:id",adminMiddleware,UpdateProblem);
-problemrouter.delete("/delete/:id",adminMiddleware,DeleteProblem); 
+problemrouter.put("/update/:id", adminMiddleware, UpdateProblem);
+problemrouter.delete("/delete/:id", adminMiddleware, DeleteProblem);
 
 // Anyone can access
-problemrouter.get("/fetchproblem/:id",authMiddleware,FetchProblem);
-problemrouter.get("/fetchallproblem",authMiddleware,FetchAllProblem);
-// problemrouter.get("/user",authMiddleware,SolvedProblem);
+problemrouter.get("/fetchproblem/:id", authMiddleware, FetchProblem);
+problemrouter.get("/fetchallproblem", authMiddleware, FetchAllProblem);
+problemrouter.get("/user-problem", authMiddleware, SolvedProblem);
+problemrouter.get("/:pid",authMiddleware,totalproblemsubmitted);
 
 export default problemrouter;

2b0b7616fe7c29da4bcdcce9f06a5bdfdf109952
Shreyasshukla957 <shuklashreyas.shs@gmail.com>
2026-06-11 13:28:11 +0530

    add delete Profile route for deleting Profile handler

--- src/routes/user.auth.ts
+++ src/routes/user.auth.ts
@@ -1,12 +1,14 @@
 import express , {Router} from "express";
 import {Register ,login , logout } from "../controllers/auth.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { deleteProfile } from "../controllers/auth.controller.js";
 
 const authRouter:Router = express.Router();
 
 authRouter.post("/Register",Register);
 authRouter.post("/login",login);
 authRouter.post("/logout",authMiddleware,logout);
+authRouter.delete("/delete-Profile/:id",deleteProfile);
 // authRouter.get("/getProfile",getProfile);
 
 export default authRouter;
